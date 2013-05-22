846c5f601efe9208df6ed6c42f45f6fb436ef306
Roy Firestein <roy@fireste.in>
2013-05-22 16:21:28 -0400

    Version 3.0 was rewritten to be object-oriented, and can handle autherization and connecion errors

--- auto_nessus.py
+++ auto_nessus.py
@@ -2,11 +2,11 @@
 '''
 Nessus-0-Matic
 
-v2.0 
-29/1/2012
+Version: 3.0
+12/5/2012
 
 
-by Roy Firestein [roy@firestein.net]
+by Roy Firestein [roy**AT**firestein.net]
 
 
 REQUIREMENTS:
@@ -19,23 +19,7 @@ Tested with Nessus 5.
 
 '''
 
-
-## EDIT THIS ##
-infile = "targets.txt"
-outdir = "./"
-
-project_name = "testing"
-
-server = "localhost"
-port = "8834"
-user = "admin"
-passwd = "admin"
-## STOP EDITING ##
-
-
-
-
-import sys
+import sys, os
 import time
 try:
 	import ipaddr
@@ -46,99 +30,226 @@ except ImportError:
 try:
 	import nessus
 except ImportError:
-	print "Error:\npython-nessus missing.\nInstall using: 'pip install python-nessus'\n"
+	print "Error:\npython-nessus missing.\nInstall using: 'pip install nessus'\n"
 	sys.exit(0)
 
 
-nessuslist = []
-
-print "\tauto_nessus | Nessus Automation script"
-
-def run(policyid):
-
-	netlist = open(infile, 'r').readlines()
-	nessuslist = []
-
-	for net in netlist:
-		subnet = ipaddr.IPv4Network(net)
-		v4list = [str(x) for x in subnet if x._version == 4]
+class AutoNessus(nessus.NessusConnection):
+	
+	
+	def __init__(self, username, password, url='https://localhost:8834', **kwargs):
+		super(AutoNessus, self).__init__(username, password, url)
+		self.raw_targets = []
+		self.targets = []
+		self.reports = []
+		self.policies = []
+		self.scan = None
+		self.name = kwargs.get("name", "scan")
+		self.output_dir = kwargs.get("output_dir", "./")
+		self.group_size = kwargs.get("group_size", 16)
+		self.verbose = kwargs.get("verbose", False)
+		self.index_counter = kwargs.get("index_counter", 0)
+	
+	def start_scan(self, policy_id):
+		if not len(self.targets) > 0:
+			print "No Targets Found."
+			return False
 		
-		for chunk in chunks(v4list, 16):
-			nessuslist.append(chunk)
-
-	nes = nessus.NessusConnection(user , passwd)
-	print "[+] Connected to Nessus server"
-	print "[+] %d IP groups to scan" %(len(nessuslist))
-
-	i=0
-	for group in nessuslist:
-		print "[*] Starting new scan against: %s to %s" %(group[0], group[-1])
-		name = "%s_%s_%s_to_%s" %(project_name, i, group[0], group[-1])
-		scan = nes.create_scan(policyid, name, group)
 		while True:
-			uuid = scan.uuid
-			reports = nes.list_reports()
-			report = None
-			for r in reports:
-				if r.name == uuid:
-					report = r
-					break
-			if report.status == "running":
-				time.sleep(60)
-			elif report.status == "completed":
+			self.check_auth()
+			
+			### Create new sub scan
+			group = self.targets[self.index_counter]
+			self.print_yellow("[%s] Starting new scan against: %s to %s" %(self.index_counter, group[0], group[-1]))
+			name = "%s_%s_%s_to_%s" %(self.name, self.index_counter, group[0], group[-1])
+			self.scan = self.create_scan(policy_id, name, group)
+			
+			### Check if scan is still running
+			while True:
+				report = self.get_scan_report()
+				if report:
+					if self.verbose:
+						print "[v] Scan status: %s" %(report.status)
+					if report.status == "running":
+						time.sleep(60)
+					elif report.status == "completed":
+						break
+			
+			### Save report
+			self.save_report(self.scan.uuid, name)
+			
+			### Stuff
+			self.increment_counter()
+			if self.all_scans_are_finished():
+				self.print_green("[*] Scan completed.")
+				self.send_notification()
 				break
-		fname = outdir + name + ".nessus"
+		
+		self.print_blue("[+] Finished scanning all targets!")
+		self.print_blue("[+] Good day.")
+		return True
+	
+	def check_auth(self, force=False):
+		if not self._authenticated or force:
+			while True:
+				try:
+					if self.verbose:
+						print "Trying to authenticate."
+					self._authenticate()
+					if self._authenticated:
+						break
+				except Exception as err:
+					if self.verbose:
+						print "Authentication failed."
+						print err
+					pass
+		return self._authenticated
+	
+	def get_reports(self):
+		self.check_auth()
+		while True:
+			try:
+				self.reports = self.list_reports()
+				break
+			except Exception as err:
+				if self.verbose:
+					print err
+				self.check_auth(force=True)
+		return self.reports
+	
+	def get_policies(self):
+		self.check_auth()
+		while True:
+			try:
+				self.policies = self.list_policies()
+				break
+			except Exception as err:
+				if self.verbose:
+					print err
+				self.check_auth(force=True)
+		return self.policies
+	
+	def parse_targets(self, targets_file):
+		self.raw_targets = open(targets_file, 'r').readlines()
+		for net in self.raw_targets:
+			subnet = ipaddr.IPv4Network(net)
+			v4list = [str(x) for x in subnet if x._version == 4]
+			for chunk in self.chunks(v4list, self.group_size):
+				self.targets.append(chunk)
+	
+	def save_report(self, scan_uuid, file_name):
+		fname = "%s%s.nessus" %(self.output_dir, file_name)
 		outfile = open(fname , 'w')
-		report = nes.download_report(scan.uuid, outfile)
+		self.check_auth()
+		self.download_report(scan_uuid, outfile)
 		outfile.close()
-		i=i+1
-		#print "[*] Finished scanning tagets: %s to %s" %(group[0], group[-1])
+		
+	def increment_counter(self):
+		self.index_counter = self.index_counter + 1
+		
+	def all_scans_are_finished(self):
+		if len(self.targets) == self.index_counter:
+			return True
+		return False
 	
-	print "[+] Finished scanning all targets!"
-	print "[+] Good day."
+	def get_scan_report(self):
+		reports = self.get_reports()
+		for r in reports:
+			if r.name == self.scan.uuid:
+				return r
+		return None
+	
+	def print_policies(self):
+		policies = self.get_policies()
+		print "ID\tPolicy name"
+		for key in policies:
+			print "%s\t%s" %(key.id, key.name)
+		print
+		
+	def print_scans(self):
+		reports = self.get_reports()
+		print "Status\tUUID"
+		for report in reports:
+			print "%s\t%s" %(report.status, report.name)
+		print
+	
+	def chunks(self, l, n):
+		return [l[i:i+n] for i in range(0, len(l), n)]
 
-
-
-def get_policies():
-	nes = nessus.NessusConnection(user , passwd)
-	policies = nes.list_policies()
-	print "ID\tPolicy name"
-	for key in policies:
-		print "%s\t%s" %(key.id, key.name)
+	def send_notification(self):
+		os.system("curl -s fireste.in/ping/nessusscan/")
+		print "Notification sent."
+	
+	def print_blue(self, text):
+		print '\033[94m' + "%s" %text + '\033[0m'
+		
+	def print_yellow(self, text):
+		print '\033[93m' + "%s" %text + '\033[0m'
+	
+	def print_green(self, text):
+		print '\033[92m' + "%s" %text + '\033[0m'
+	
+	
+	
+	
+		
+def main():
+	"""
+	
+	Main
+	
+	"""
+	print
+	print "\tauto_nessus | Nessus Automation script"
 	print
 
-def get_scans():
-	nes = nessus.NessusConnection(user , passwd)
-	reports = nes.list_reports()
-	print "Status\tUUIS"
-	for report in reports:
-		print "%s\t%s" %(report.status, report.name)
-
-def chunks(l, n):
-	return [l[i:i+n] for i in range(0, len(l), n)]
-
-def usage():
-	print "Usage: "
-	print "%s --list" %(sys.argv[0])
-	print "%s --scan policyID" %(sys.argv[0])
-	print
-
-
-
-
-
-if sys.argv.__len__() > 1:
-	if sys.argv[1] == "--list":
-		get_policies()
-	elif sys.argv[1] == "--scan":
-		if sys.argv.__len__() != 3:
-			usage()
-		else:
-			run(sys.argv[2])
-	elif sys.argv[1] == "--running":
-		get_scans()
-else:
-	usage()
+	from optparse import OptionParser, OptionGroup
+	usage_text = "usage: %prog [options] list | scan"
+	parser = OptionParser(usage_text)
+	extra = OptionGroup(parser, "Extra options")
+	parser.add_option("-t", "--targets",  action="store", type="string", dest="targets", help="Targets file", default="targets.txt")
+	parser.add_option("-u", "--user",  action="store", type="string", dest="username", help="Nessus username", default="admin")
+	parser.add_option("-w", "--pass",  action="store", type="string", dest="password", help="Nessus password")
+	parser.add_option("-p", "--port",  action="store", type="string", dest="port", help="Nessus port", default="8834")
+	parser.add_option("-s", "--server",  action="store", type="string", dest="server", help="Nessus host", default="localhost")
+	extra.add_option("-n", "--name",  action="store", type="string", dest="name", help="File name", default="scan")
+	extra.add_option("-d", "--dir",  action="store", type="string", dest="directory", help="Output directory", default="./")
+	extra.add_option("-g", "--group",  action="store", type="int", dest="group", help="Group size of IPs to scan", default=16)
+	extra.add_option("-i", "--index",  action="store", type="int", dest="index", help="Scan index starts from 0", default=0)
+	extra.add_option("-v", "--verbose",  action="store_true", dest="verbose", help="Show extra information")
+	parser.add_option_group(extra)
+	(menu, args) = parser.parse_args()
 	
+	if len(args) < 1:
+		parser.error("incorrect number of arguments")
+	
+	server = menu.server
+	port = menu.port
+	url = "https://%s:%s" %(server, port)
+	scanner = AutoNessus(menu.username, menu.password, url)
+	scanner.verbose = menu.verbose
+
+	if len(args) >= 1:
+		if args[0] == "list":
+			scanner.print_policies()
+		elif args[0] == "scan":
+			if len(args) != 2:
+				parser.print_usage()
+			else:
+				scanner.group_size = menu.group
+				scanner.index_counter = menu.index
+				scanner.name = menu.name
+				scanner.output_dir = menu.directory
+				scanner.parse_targets(menu.targets)
+				policy_id = args[1]
+				scanner.start_scan(policy_id)
+				
+		elif args[0] == "running":
+			scanner.print_scans()
+	else:
+		parser.print_usage()
+
+if __name__ == "__main__":
+	main()
 	
 exit
