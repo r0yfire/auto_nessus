fdf2f1f34a51498d24ccdc60f0beda5ec3528c61
Roy Firestein <roy@fireste.in>
2014-05-03 14:13:07 -0400

    Warn when duplicate ranges are combined. Enforce UTF-8 encoding.

--- joinessus.py
+++ joinessus.py
@@ -22,7 +22,20 @@ parser.add_option("-f", "--first",  action="store", type="string", dest="first",
 parser.add_option("-d", "--dir",  action="store", type="string", dest="dir", help="Directory containing .nessus files")
 parser.add_option("-o", "--output",  action="store", type="string", dest="output", help="output file name")
 parser.add_option("-n", "--name",  action="store", type="string", default="combined scan", dest="name", help="New report name")
+parser.add_option("-v", "--verbose",  action="store_true", dest="verbose", help="Show extra information")
 (menu, args) = parser.parse_args()
+
+
+def parse_targets(domxml, set_value=None):
+	pref = domxml.getElementsByTagName('ServerPreferences')[0]
+	for p in pref.childNodes:
+		for c in p.childNodes:
+			if c.nodeType == c.ELEMENT_NODE and c.childNodes[0].data == 'TARGET':
+				if set_value:
+					c.nextSibling.nextSibling.childNodes[0].data = set_value
+					return domxml
+				else:
+					return c.nextSibling.nextSibling.childNodes[0].data.split(",")
 
 
 if menu.first and menu.dir and menu.output:
@@ -31,6 +44,7 @@ if menu.first and menu.dir and menu.output:
 	dir_nessus = os.path.realpath(menu.dir)
 	nessus_files = os.listdir(dir_nessus)
 	scan_result = []
+	targets = []
 	counter = 0
 	
 	if nessus_files.__contains__(os.path.basename(files[0])):
@@ -43,6 +57,10 @@ if menu.first and menu.dir and menu.output:
 		if counter == 0:
 			# first Nessus scan
 			first_dom = xml.dom.minidom.parseString(nessus_xml)
+			
+			# Save report scope
+			targets = parse_targets(first_dom)
+				
 			if menu.name:
 				first_dom.getElementsByTagName('Report')[0].setAttribute('name', menu.name)
 		else:
@@ -55,13 +73,24 @@ if menu.first and menu.dir and menu.output:
 				continue
 			for host in dom.getElementsByTagName('ReportHost'):
 				scan_result.append(host)
+			# Save targets
+			target = parse_targets(dom)
+			for t in target:
+				if t not in targets:
+					targets.append(t)
+				else:
+					print "Duplicate range (%s) found in %s" %(t, nes_file)
 		counter=counter+1
 	
 	for node in scan_result:
 		first_dom.getElementsByTagName('Report')[0].appendChild(node)
-		
+	
+	# Update TARGETS perferences
+	first_dom = parse_targets(first_dom, set_value=",".join(targets))
+	
+	# Save new combined report
 	fh = open(menu.output, 'w')
-	first_dom.writexml(fh)
+	fh.write(first_dom.toxml(encoding="utf-8"))
 	fh.close()
 else:
 	parser.print_help()
